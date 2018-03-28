75a41c2606ca7beaa8a78fd109784cc7ff30eb2d
Nigel Henshaw <nigel@mobapptuts.com>
2018-03-28 11:16:19 +0900

    Installing package demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new HelloWorldApp());
 
 class HelloWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to hello world app',
       home: new Scaffold(
@@ -12,7 +14,13 @@ class HelloWorldApp extends StatelessWidget {
           title: new Text('Hello Flutter App'),
         ),
         body: new Center(
-          child: new Text('Hello Flutter'),
+//          child: new Text('Hello Flutter'),
+          child: new Text(
+            wordPair.asPascalCase,
+            style: new TextStyle(
+                fontSize: 36.0
+            ),
+          ),
         ),
       ),
     );
